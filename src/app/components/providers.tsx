050cf3e38ac601378d58c339fd95e97afe5d0ada
AustinLCouch <AustinLCouch@gmail.com>
2025-08-21 01:13:43 -0400

    fix: update providers

--- src/app/components/providers.tsx
+++ src/app/components/providers.tsx
@@ -6,11 +6,6 @@ type Props = {
   children?: React.ReactNode;
 };
 
-export const Providers = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>;
-};
-
-// Also export as NextAuthProvider for compatibility
 export const NextAuthProvider = ({ children }: Props) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
